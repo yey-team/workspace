d061967483943ae9ea7e5cb6694721eefc4338be
Ilan <71207643+IlanOu@users.noreply.github.com>
2024-01-09 21:04:59 +0000

    image bloc

--- src/components/Blocs/ImageBloc.vue
+++ src/components/Blocs/ImageBloc.vue
@@ -1,14 +1,43 @@
 <template>
 
-    Bonjour ! je suis une image !
-      
-  </template>
-  
-  <script setup>
-  
-  </script>
-  
-  <style scoped>
+  <div class='resizable' @mousedown.stop>
+    <img class="image" src="https://imgupscaler.com/images/samples/animal-after.webp">
+  </div>
 
-  </style>
+</template>
+  
+<script setup>
+
+</script>
+  
+<style scoped>
+  
+  .resizable {
+    display: inline-block;
+    background: red;
+    resize: both;
+    overflow: hidden;
+    line-height: 0;
+
+    min-width: 200px;
+    max-width: 1000px;
+
+    min-height: 100px;
+
+    width: max-content !important;
+
+
+  }
+
+
+  .image {
+    min-width: 200px;
+    max-width: 1000px;
+
+    width: 100%;
+    height: 100%;
+
+    object-fit: cover;
+  }
+</style>
   
