889eee5f58067504c7a61ec42752c5572c32104b
math-pixel <mathcb74@gmail.com>
2024-01-30 14:53:01 +0100

    add pinia store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import './assets/base.css'
 import './assets/style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+
+app.mount('#app')
