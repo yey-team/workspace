1c306fadb0384eb4d48a99343ae174704d3ab659
Ilan Outhier <ilan@hyperfiction.fr>
2024-01-31 16:23:50 +0100

    clear to test

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,155 +1,25 @@
 <template>
-  <div
-    class="container"
-    :style="{ top: blockPosition.y + 'px', left: blockPosition.x + 'px', zIndex: index}"
-    @mousedown="startDrag"
-    @mouseup="() => { stopDrag(); $emit('onUpdate', {id: props.id, position: {x:blockPosition.x, y:blockPosition.y}}); }"
-  >
-    <!-- Votre contenu personnalisé -->
-    {{ content }}
 
-    <br>
+<div class="block">
+    {{ testBlock.type }}
+</div>
 
-    <component :is="selectedComponent" @selected="setBlockTop" @unselected="unsetBlockTop"/>
-
-  </div>
 </template>
 
 <script setup>
-import { ref, shallowRef, onMounted, onBeforeUnmount } from 'vue';
-import TextBlock from './Blocks/TextBlock.vue';
-import ImageBlock from './Blocks/ImageBlock.vue';
+import { ref } from 'vue';
+import { blocks } from '@/helpers/blockHelper.js';
 
-let magnetEffectSize = 20;
+const localBlocks = JSON.parse(JSON.stringify(blocks));
 
-const props = defineProps(['id', 'position', 'content', 'scale', 'type']);
-const blockPosition = ref({ x: props.position.x, y: props.position.y });
-
-const emit = defineEmits()
-
-/* -------------------------------------------------------------------------- */
-/*                             Variable Component                             */
-/* -------------------------------------------------------------------------- */
-
-const listOfComponents = shallowRef({"text": TextBlock, 
-                                      "image": ImageBlock
-                                    })
-
-const selectedComponent = shallowRef(null);
-
-// Fonction pour changer le composant sélectionné
-function selectComponentToCreate() {
-  selectedComponent.value = listOfComponents.value[props.type];
-};
-
-
-
-/* -------------------------------------------------------------------------- */
-/*                                Z Index blocks                               */
-/* -------------------------------------------------------------------------- */
-
-
-const index = ref(0);
-
-function setBlockTop(){
-  index.value = 10;
-}
-
-function unsetBlockTop(){
-  index.value = 0;
-}
-
-
-
-/* -------------------------------------------------------------------------- */
-/*                               Dragging system                              */
-/* -------------------------------------------------------------------------- */
-
-
-const isDragging = ref(false);
-const lastMousePosition = ref({ x: 0, y: 0 });
-
-
-
-
-onMounted(() => {
-  document.addEventListener('mousemove', handleDrag);
-  selectComponentToCreate()
-});
-
-onBeforeUnmount(() => {
-  document.removeEventListener('mousemove', handleDrag);
-});
-
-function startDrag(event) {
-  if (event.button === 0) {
-    // Is dragging
-    setBlockTop()
-
-    isDragging.value = true;
-    lastMousePosition.value = { x: event.clientX, y: event.clientY };
-  }
-};
-
-function updatePosition(newPosition) {
-  blockPosition.value = newPosition;
-};
-
-function handleDrag(event) {
-  if (isDragging.value) {
-    const deltaX = event.movementX || (event.clientX - lastMousePosition.value.x);
-    const deltaY = event.movementY || (event.clientY - lastMousePosition.value.y);
-
-    const speedFactor = 1 / props.scale;
-
-    const newBlockPosition = {
-      x: blockPosition.value.x + (deltaX * speedFactor),
-      y: blockPosition.value.y + (deltaY * speedFactor),
-    };
-
-    updatePosition(newBlockPosition);
-
-    lastMousePosition.value = { x: event.clientX, y: event.clientY };
-  }
-};
-
-function stopDrag(){
-  if (isDragging.value) {
-    // Is not dragging
-    unsetBlockTop()
-
-    isDragging.value = false;
-
-    const newBlockPosition = {
-      x: Math.round(blockPosition.value.x / magnetEffectSize ) * magnetEffectSize,
-      y: Math.round(blockPosition.value.y / magnetEffectSize ) * magnetEffectSize,
-    }
-
-    updatePosition(newBlockPosition)
-  }
-};
+let testBlock = localBlocks[0];
 </script>
 
 <style scoped>
-.container {
-  position: absolute;
-  background-color: var(--main-div-color);
-  padding: 1rem;
-  border-radius: var(--large-border-radius);
-  border: 1px solid var(--border-color);
-  box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.25);
-  
-  width: auto;
-  height: auto;
 
-  min-width: 200px;
-  min-height: 50px;
-
-  cursor: grab;
-  transition: transform 0.2s ease-out;
+.block {
+    padding: 1rem;
+    background: #fff2;
 }
 
-.container:active {
-  cursor: grabbing;
-}
 </style>
--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,40 +1,9 @@
 <template>
-    <div class="work-plan" @mousedown="startDrag" @mousemove="handleDrag" @mouseup="stopDrag" @mouseleave="stopDrag" @wheel.prevent="handleZoom" v-on:click.right="openMenu($event)" v-on:click.left="closeMenu()">
+    <div class="work-plan" @mousedown="startDrag" @mousemove="handleDrag" @mouseup="stopDrag" @mouseleave="stopDrag" @wheel.prevent="handleZoom">
       <div class="content" :style="{ transform: `translate(${translate.x}px, ${translate.y}px) scale(${scale})` }">
         <!-- Contenu de votre plan de travail -->
-        <Block
-          v-for="(block, index) in allBlock"
-          :id="block.id"
-          :position="block.position"
-          :content="block.content"
-          :scale="scale"
-          :type="block.type"
-          @onUpdate="updateBlock"
-        />
-
-        <button @click="() => {newBlock('text'); updatePosition();}">
-          Ajouter un bloc de text
-        </button>
-        <button @click="newBlock('image')">
-          Ajouter un bloc d'image
-        </button>
-        
-        
-        <button @click="goToCoords(500,500)">
-          Aller en 500x500
-        </button>
-
-        <button @click="goToBlock('0')">
-          Aller à la boite n°1
-        </button>
-        
-        
-        <Arrow :startPoint="allBlock[0].position" :endPoint="allBlock[1].position"/>
+        <Block/>
       </div>
-
-
-      <Menu :style="{ top: `${yPointMenu}px` , left: `${xPointMenu}px`, transform: `scale(${xScaleMenu}, ${yScaleMenu})` }" :configMenu="configMenu" />
-
 
     </div>
   </template>
@@ -44,72 +13,16 @@
     import { ref } from 'vue';
 
     //import Block from './Block.vue';
-    import Menu from './Menu/MenuContainer.vue';
     import Block from './Block.vue';
-    import Arrow from './Arrow.vue';
 
-
-    // const blocks = getBlocks()
-    import { blocks, newBlock } from '@/helpers/blockHelper.js';
-    import {configsMenu} from '@/helpers/configMenu.js'
-
-    //Import store
-    import { Store } from '@/store.js'
-
-
-    
-    
-    const allBlock = ref(blocks)
-    
-    const xPointMenu = ref(0)
-    const yPointMenu = ref(0)
-    let configMenu = ref([])
-    const xScaleMenu = ref(0)
-    const yScaleMenu = ref(0)
-
-    
-    
     const isDragging = ref(false);
-    
     const lastMousePosition = ref({x: 0,y: 0});
-    
     const translate = ref({x: 0,y: 0});
-    
     const scale = ref(1);
-    
+
     const velocity = ref({x: 0,y: 0});
     
 
-
-
-    /* -------------------------------------------------------------------------- */
-    /*                                Move to view                                */
-    /* -------------------------------------------------------------------------- */
-
-
-    function getBlockByID(id){
-
-      const blockIndex = allBlock.value.findIndex(block => block.id === id);
-      return allBlock.value[blockIndex];
-    }
-
-
-    function goToCoords(x=0, y=0) {
-      x = -x * scale.value;
-      y = -y * scale.value;
-      translate.value = {x, y}
-    }
-    
-    function goToBlock(blockId) {
-      const block = getBlockByID(blockId)
-      goToCoords(block.position.x,block.position.y);
-    }
- 
-
-    function updateBlock(attributes) {
-      const block = getBlockByID(attributes.id)
-      block.position = attributes.position
-    }
 
 
     /* -------------------------------------------------------------------------- */
@@ -192,41 +105,8 @@
       translate.value.x = (translate.value.x - centerX) * (clampedScale / scale.value) + centerX;
       translate.value.y = (translate.value.y - centerY) * (clampedScale / scale.value) + centerY;
 
-      // Mettre à jour l'échelle
       scale.value = clampedScale;
     };
-
-
-
-    /* -------------------------------------------------------------------------- */
-    /*                                 Right Click                                */
-    /* -------------------------------------------------------------------------- */
-
-    function openMenu(event){
-
-      //? Remove basic menu  
-      event.preventDefault()
-
-      xScaleMenu.value = 1
-      yScaleMenu.value = 1
-
-      // console.log(event)
-      this.xPointMenu = event.clientX
-      this.yPointMenu = event.clientY
-
-      //save position of the mouse in store
-      Store.mouseX = event.clientX
-      Store.mouseY = event.clientY
-
-      configMenu = configsMenu.mainMenu;
-
-      // blockManager.newBlock("image")
-    }
-
-    function closeMenu(){
-      xScaleMenu.value = 0
-      yScaleMenu.value = 0
-    }
 
     
   </script>
@@ -238,13 +118,10 @@
       overflow: hidden;
       position: relative;
       transition: transform 0.2s ease-out;
-      /* Ajoutez une transition pour une expérience plus fluide */
       cursor: grab;
-      /* Changement du curseur pour indiquer que le déplacement est activé */
     }
     .work-plan:active {
       cursor: grabbing;
-      /* Changement du curseur lorsque le clic est enfoncé */
     }
     .content {
       position: absolute;
