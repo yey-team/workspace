78e7e5fc0c766154057003342fea84fabb5c81d0
Pierre-Louis ROUSSEAUX <pl74rou@gmail.com>
2024-02-07 21:18:46 +0100

    change uuid creation

--- api/api.php
+++ api/api.php
@@ -163,8 +163,7 @@ function tokenCreation($userid)
 
 function generateUniqueId()
 {
-    $data = PHP_MAJOR_VERSION < 7 ? openssl_random_pseudo_bytes(16) : random_bytes(16);
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    $data = PHP_MAJOR_VERSION < 7 ? openssl_random_pseudo_bytes(8) : random_bytes(8);
+    $id = unpack('Q', $data)[1];
+    return $id;
 }
