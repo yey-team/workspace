bad80cfc49150b6a50ee96d676c7b858870df7bd
Nay <ilan.outyey@gmail.com>
2024-02-26 07:17:36 +0100

    remove defineEmits import

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -48,7 +48,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineEmits } from 'vue';
+import { ref } from 'vue';
 const isLoginPage = ref(true);
 
 const emits = defineEmits(['loginSuccess']);
